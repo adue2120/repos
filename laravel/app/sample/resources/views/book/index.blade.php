08d97353a164d659a6b750200d7865d207d32626
adue2120 <adue2120@gmail.com>
2022-05-14 15:16:29 +0900

    初めてのコミット
    LaravelアプリとDB

---
+++ laravel/app/sample/resources/views/book/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        @foreach ($books as $book)
+        <tr>
+            <td>{{ $book->id }}</td>
+            <td>{{ $book->title }}</td>
+            <td>{{ $book->inert_timestamp }}</td>
+        </tr>
+        @endforeach
+    </body>
+</html>
---
+++ laravel/db/init/01_create_table.sql
@@ -0,0 +1,5 @@
+create table books( 
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(100), 
+    insert_timestamp DATETIME DEFAULT NULL 
+);
